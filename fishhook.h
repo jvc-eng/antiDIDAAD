e3bd03cc2b06eda95f1d5fcea67b0d13bfe595ed
jvc-eng <70835122+jvc-eng@users.noreply.github.com>
2026-07-31 13:17:43 +0800

    Create fishhook.h

---
+++ fishhook.h
@@ -0,0 +1,27 @@
+#ifndef FISHHOOK_H
+#define FISHHOOK_H
+
+#import <mach-o/loader.h>
+#import <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct rebinding {
+  const char *name;
+  void *replacement;
+  void **replaced;
+};
+
+int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
+int rebind_symbols_image(void *header,
+                         intptr_t slide,
+                         struct rebinding rebindings[],
+                         size_t rebindings_nel);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
