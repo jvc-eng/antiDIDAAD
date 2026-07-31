6caf13d375ccdcdc5e001f2c3b43420048bcf524
jvc-eng <70835122+jvc-eng@users.noreply.github.com>
2026-07-31 13:47:04 +0800

    Update fishhook.c

--- fishhook.c
+++ fishhook.c
@@ -1,186 +1,121 @@
-#include "fishhook.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <netdb.h>
+#import "fishhook.h"
 
-#include <dlfcn.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#import <mach-o/dyld.h>
-#import <mach-o/loader.h>
-#import <mach-o/nlist.h>
+// ==================== 1. 域名拦截部分 ====================
 
-#ifdef __LP64__
-typedef struct mach_header_64 mach_header_t;
-typedef struct segment_command_64 segment_command_t;
-typedef struct load_command load_command_t;
-typedef struct section_64 section_t;
-typedef struct nlist_64 nlist_t;
-#define LC_SEGMENT_ARCH_DEPENDENT LC_SEGMENT_64
-#else
-typedef struct mach_header mach_header_t;
-typedef struct segment_command segment_command_t;
-typedef struct load_command load_command_t;
-typedef struct section section_t;
-typedef struct nlist nlist_t;
-#define LC_SEGMENT_ARCH_DEPENDENT LC_SEGMENT
-#endif
+static NSArray<NSString *> *blockedSuffixes;
+static int (*orig_getaddrinfo)(const char *hostname, const char *servname, const struct addrinfo *hints, struct addrinfo **res);
 
-#ifndef SEG_DATA_CONST
-#define SEG_DATA_CONST "__DATA_CONST"
-#endif
-
-struct rebindings_entry {
-  struct rebinding *rebindings;
-  size_t rebindings_nel;
-  struct rebindings_entry *next;
-};
-
-static struct rebindings_entry *_rebindings_head = NULL;
-
-static int prepend_rebindings(struct rebindings_entry **default_entry,
-                              struct rebinding rebindings[],
-                              size_t nel) {
-  struct rebindings_entry *new_entry = (struct rebindings_entry *)malloc(sizeof(struct rebindings_entry));
-  if (!new_entry) {
-    return -1;
-  }
-  new_entry->rebindings = (struct rebinding *)malloc(sizeof(struct rebinding) * nel);
-  if (!new_entry->rebindings) {
-    free(new_entry);
-    return -1;
-  }
-  memcpy(new_entry->rebindings, rebindings, sizeof(struct rebinding) * nel);
-  new_entry->rebindings_nel = nel;
-  new_entry->next = *default_entry;
-  *default_entry = new_entry;
-  return 0;
-}
-
-static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
-                                           section_t *section,
-                                           intptr_t slide,
-                                           nlist_t *symtab,
-                                           char *strtab,
-                                           uint32_t *indirect_symtab) {
-  uint32_t *indirect_symbol_indices = indirect_symtab + section->reserved1;
-  void **indirect_symbol_bindings = (void **)((uintptr_t)slide + section->addr);
-  for (uint32_t i = 0; i < section->size / sizeof(void *); i++) {
-    uint32_t symtab_index = indirect_symbol_indices[i];
-    if (symtab_index == INDIRECT_SYMBOL_ABS || symtab_index == INDIRECT_SYMBOL_LOCAL ||
-        symtab_index == (INDIRECT_SYMBOL_LOCAL | INDIRECT_SYMBOL_ABS)) {
-      continue;
-    }
-    uint32_t strtab_offset = symtab[symtab_index].n_un.n_strx;
-    char *symbol_name = strtab + strtab_offset;
-    bool symbol_name_longer_than_one = symbol_name[0] && symbol_name[1];
-    struct rebindings_entry *cur = rebindings;
-    while (cur) {
-      for (uint32_t j = 0; j < cur->rebindings_nel; j++) {
-        if (symbol_name_longer_than_one && strcmp(&symbol_name[1], cur->rebindings[j].name) == 0) {
-          if (cur->rebindings[j].replaced != NULL &&
-              indirect_symbol_bindings[i] != cur->rebindings[j].replacement) {
-            *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
-          }
-          indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
-          goto symbol_cleanup;
+static int my_getaddrinfo(const char *hostname, const char *servname, const struct addrinfo *hints, struct addrinfo **res) {
+    if (hostname != NULL) {
+        @autoreleasepool {
+            NSString *hostStr = [NSString stringWithUTF8String:hostname];
+            if (hostStr) {
+                hostStr = [hostStr lowercaseString];
+                for (NSString *suffix in blockedSuffixes) {
+                    if ([hostStr isEqualToString:suffix] || [hostStr hasSuffix:[@"." stringByAppendingString:suffix]]) {
+                        return EAI_NONAME;
+                    }
+                }
+            }
         }
-      }
-      cur = cur->next;
     }
-  symbol_cleanup:;
-  }
+    return orig_getaddrinfo(hostname, servname, hints, res);
 }
 
-static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
-                                     const struct mach_header *header,
-                                     intptr_t slide) {
-  Dl_info info;
-  if (dladdr(header, &info) == 0) {
-    return;
-  }
+// ==================== 2. 广告弹窗视图拦截 (OC Hook) ====================
 
-  segment_command_t *cur_seg_cmd;
-  segment_command_t *linkedit_segment = NULL;
-  struct symtab_command* symtab_cmd = NULL;
-  struct dysymtab_command* dysymtab_cmd = NULL;
+// 2.1 强行禁止广告 Controller 弹出 Presentation
+%hook UIViewController
 
-  uintptr_t cur = (uintptr_t)header + sizeof(mach_header_t);
-  for (uint32_t i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
-    cur_seg_cmd = (segment_command_t *)cur;
-    if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
-      if (strcmp(cur_seg_cmd->segname, SEG_LINKEDIT) == 0) {
-        linkedit_segment = cur_seg_cmd;
-      }
-    } else if (cur_seg_cmd->cmd == LC_SYMTAB) {
-      symtab_cmd = (struct symtab_command*)cur_seg_cmd;
-    } else if (cur_seg_cmd->cmd == LC_DYSYMTAB) {
-      dysymtab_cmd = (struct dysymtab_command*)cur_seg_cmd;
-    }
-  }
-
-  if (!symtab_cmd || !dysymtab_cmd || !linkedit_segment ||
-      !dysymtab_cmd->nindirectsyms) {
-    return;
-  }
-
-  uintptr_t linkedit_base = (uintptr_t)slide + linkedit_segment->vmaddr - linkedit_segment->fileoff;
-  nlist_t *symtab = (nlist_t *)(linkedit_base + symtab_cmd->symoff);
-  char *strtab = (char *)(linkedit_base + symtab_cmd->stroff);
-
-  uint32_t *indirect_symtab = (uint32_t *)(linkedit_base + dysymtab_cmd->indirectsymoff);
-
-  cur = (uintptr_t)header + sizeof(mach_header_t);
-  for (uint32_t i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
-    cur_seg_cmd = (segment_command_t *)cur;
-    if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
-      if (strcmp(cur_seg_cmd->segname, SEG_DATA) != 0 &&
-          strcmp(cur_seg_cmd->segname, SEG_DATA_CONST) != 0) {
-        continue;
-      }
-      for (uint32_t j = 0; j < cur_seg_cmd->nsects; j++) {
-        section_t *sect = (section_t *)(cur + sizeof(segment_command_t)) + j;
-        if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS ||
-            (sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {
-          perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
+- (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion {
+    NSString *className = NSStringFromClass([viewControllerToPresent class]);
+    
+    // 常见的广告 SDK 视图控制器前缀
+    if ([className containsString:@"BUNative"] ||      // 穿山甲 Pangle
+        [className containsString:@"BUAd"] ||          // 穿山甲 Pangle
+        [className containsString:@"BUSplash"] ||      // 穿山甲开屏
+        [className containsString:@"BUExpress"] ||     // 穿山甲模板
+        [className containsString:@"GDT"] ||           // 广点通/优量汇
+        [className containsString:@"BaiduMobAd"] ||    // 百度广告
+        [className containsString:@"KSAd"] ||          // 快手广告
+        [className containsString:@"Atmos"] ||         // AnyThink/TopOn
+        [className containsString:@"ADJ"] ||           // Adjust
+        [className containsString:@"AdViewController"]) {
+        
+        // 直接丢弃，不弹出广告
+        if (completion) {
+            completion();
         }
-      }
+        return;
     }
-  }
+    
+    %orig(viewControllerToPresent, flag, completion);
 }
 
-static void _rebind_symbols_for_image(const struct mach_header *header,
-                                      intptr_t slide) {
-    rebind_symbols_for_image(_rebindings_head, header, slide);
-}
+%end
 
-int rebind_symbols_image(void *header,
-                         intptr_t slide,
-                         struct rebinding rebindings[],
-                         size_t rebindings_nel) {
-    struct rebindings_entry *rebindings_head = NULL;
-    int retval = prepend_rebindings(&rebindings_head, rebindings, rebindings_nel);
-    if (retval < 0) {
-        return retval;
-    }
-    rebind_symbols_for_image(rebindings_head, (const struct mach_header *)header, slide);
-    free(rebindings_head->rebindings);
-    free(rebindings_head);
-    return retval;
-}
+// 2.2 Hook 穿山甲 (Pangle) 广告请求与展示（截图中样式的典型来源）
+%hook BUNativeExpressInterstitialAd
+- (BOOL)isAdValid { return NO; }
+- (void)showAdFromRootViewController:(id)arg1 { return; }
+%end
 
-int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
-  int retval = prepend_rebindings(&_rebindings_head, rebindings, rebindings_nel);
-  if (retval < 0) {
-    return retval;
-  }
-  if (!_rebindings_head->next) {
-    _dyld_register_func_for_add_image(_rebind_symbols_for_image);
-  } else {
-    uint32_t c = _dyld_image_count();
-    for (uint32_t i = 0; i < c; i++) {
-      _rebind_symbols_for_image(_dyld_get_image_header(i), _dyld_get_image_vmaddr_slide(i));
+%hook BUNativeExpressFullscreenVideoAd
+- (BOOL)isAdValid { return NO; }
+- (void)showAdFromRootViewController:(id)arg1 { return; }
+%end
+
+// 2.3 Hook 腾讯优量汇 (GDT) 插屏/视频广告
+%hook GDTUnifiedInterstitialAd
+- (BOOL)isAdValid { return NO; }
+- (void)presentAdFromRootViewController:(id)arg1 { return; }
+%end
+
+// ==================== 3. 初始化构造器 ====================
+
+%ctor {
+    @autoreleasepool {
+        blockedSuffixes = @[
+            @"admob.com",
+            @"adservice.google.com",
+            @"als.baidu.com",
+            @"anythinktech.com",
+            @"applovin.com",
+            @"applvn.com",
+            @"byteoversea.com",
+            @"gdt.qq.com",
+            @"gifshow.com",
+            @"googleads.g.doubleclick.net",
+            @"hm.baidu.com",
+            @"inmobi.com",
+            @"ironsrc.com",
+            @"is.com",
+            @"ksapisrv.com",
+            @"kuaishou.com",
+            @"mi.gdt.qq.com",
+            @"mobads.baidu.com",
+            @"pagead2.googlesyndication.com",
+            @"pangle.io",
+            @"pangolin-sdk-toutiao.com",
+            @"qzs.qq.com",
+            @"sentry.io",
+            @"snssdk.com",
+            @"supersonicads.com",
+            @"toponad.com",
+            @"umeng.com",
+            @"umengacs.m.taobao.com",
+            @"umengcloud.com"
+        ];
+
+        struct rebinding getaddrinfo_rebinding = {
+            "getaddrinfo",
+            (void *)my_getaddrinfo,
+            (void **)&orig_getaddrinfo
+        };
+        struct rebinding rebindings[] = { getaddrinfo_rebinding };
+        rebind_symbols(rebindings, 1);
     }
-  }
-  return retval;
 }
